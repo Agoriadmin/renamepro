cd3163b7db8bbd20b06edee72309ee07859aba1e
Personal Robot <86073822+Agoriadmin@users.noreply.github.com>
2021-08-08 23:16:43 +0530

    Update and rename display_progress.py to display_progress.py

--- helper/display_progress.py
+++ helper/display_progress.py
@@ -30,12 +30,12 @@ async def progress_for_pyrogram(
         elapsed_time = TimeFormatter(milliseconds=elapsed_time)
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
-        progress = "{0}{1}\n𝐒𝐭𝐚𝐭𝐮𝐬   : {2}%\n".format(
+        progress = "{0}{1}\n𝐒𝐭𝐚𝐭𝐮𝐬  ⚠️ : {2}%\n".format(
             ''.join([Translation.FINISHED_PROGRESS_STR for i in range(math.floor(percentage / 5))]),
             ''.join([Translation.UN_FINISHED_PROGRESS_STR for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "𝐏𝐫𝐨𝐜𝐞𝐬𝐬 : {0}  𝐎𝐟  {1}\n𝐒𝐩𝐞𝐞𝐝    : {2}/s\n𝐖𝐚𝐢𝐭𝐢𝐧𝐠 : {3}\n".format(
+        tmp = progress + "𝐏𝐫𝐨𝐜𝐞𝐬𝐬 ♻️: {0}  𝐎𝐟  {1}\n𝐒𝐩𝐞𝐞𝐝  🚀 : {2}/s\n𝐖𝐚𝐢𝐭𝐢𝐧𝐠 🚦: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
